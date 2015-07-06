5ea5bd70913f9539708588c4bf053ec9aa72efb3
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-07-06 13:26:03 +0200

    added angular dummy page

---
+++ src/main/java/pl/marczykm/AngularShopApplication.java
@@ -0,0 +1,12 @@
+package pl.marczykm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularShopApplication.class, args);
+    }
+}
