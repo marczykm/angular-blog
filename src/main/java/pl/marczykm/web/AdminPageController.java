51a93d9e4ae41aa57b418653be16967b9bb56da0
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-07-23 14:58:40 +0200

    added admin view for creating post

--- src/main/java/pl/marczykm/web/AdminPageController.java
+++ src/main/java/pl/marczykm/web/AdminPageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminPageController {
 
-    @RequestMapping("/")
+    @RequestMapping
     public String adminMainPage(){
         return "admin";
     }
