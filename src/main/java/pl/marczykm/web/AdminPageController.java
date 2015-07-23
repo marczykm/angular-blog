6e0d6bdc00c201c199f4757a8405cb8fbe84681e
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-07-23 14:04:42 +0200

    added admin view

--- src/main/java/pl/marczykm/web/AdminPageController.java
+++ src/main/java/pl/marczykm/web/AdminPageController.java
@@ -7,15 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by marcin on 21.07.15.
  */
 @Controller
-public class PagesController {
+@RequestMapping("/admin")
+public class AdminPageController {
 
     @RequestMapping("/")
-    public String index(){
-        return "index";
-    }
-
-    @RequestMapping("/admin")
     public String adminMainPage(){
         return "admin";
     }
+
+    @RequestMapping("/create")
+    public String adminCreatePage(){
+        return "create";
+    }
+
+    @RequestMapping("/posts")
+    public String adminManagePostsPage(){
+        return "manage";
+    }
+
+
 }
---
+++ src/main/java/pl/marczykm/web/AngularPageController.java
@@ -0,0 +1,18 @@
+package pl.marczykm.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by marcin on 21.07.15.
+ */
+@Controller
+public class AngularPageController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+}
