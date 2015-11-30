a16b85ee45056bcdff2c8345ae297c13d1efc099
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-11-30 16:45:24 +0100

    fixed paths

--- src/main/resources/public/js/blog/postCtrl.js
+++ src/main/resources/public/js/blog/postCtrl.js
@@ -1,6 +1,6 @@
 app.controller('BlogPostCtrl', function($scope, $stateParams, $http, $sce){
 
-    $http.get("/rest/post?id=" + $stateParams.id).
+    $http.get("rest/post?id=" + $stateParams.id).
             success(function(data, status, headers, config){
                 $scope.post = data;
                 $scope.post.content = $sce.trustAsHtml(data.content);
