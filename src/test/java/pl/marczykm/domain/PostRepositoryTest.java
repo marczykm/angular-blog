4fd02cd2e2ae97ed36f71a33397a917e403db08f
Marcin Marczyk <marcin.marczyk@capgemini.com>
2015-11-30 13:17:43 +0100

    refactored test

--- src/test/java/pl/marczykm/domain/PostRepositoryTest.java
+++ src/test/java/pl/marczykm/domain/PostRepositoryTest.java
@@ -2,14 +2,19 @@ package pl.marczykm.domain;
 
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import pl.marczykm.AngularBlogApplication;
+
+import java.util.List;
 
 /**
  * Created by MMARCZYK on 2015-11-30.
@@ -17,7 +22,11 @@ import pl.marczykm.AngularBlogApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AngularBlogApplication.class)
 @WebAppConfiguration
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"drop.sql", "create.sql", "insert_posts.sql"})
 public class PostRepositoryTest {
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Autowired
     PostRepository repository;
@@ -25,21 +34,47 @@ public class PostRepositoryTest {
     @Test
     public void findByIdOkIdTest() {
         Post result = repository.findById(1L);
-        Assert.assertNotNull(result);
         Assert.assertEquals(new Long(1), result.getId());
     }
 
     @Test
     public void findByIdOkAuthorTest() {
         Post result = repository.findById(1L);
-        Assert.assertNotNull(result);
         Assert.assertEquals("Anna Wróblewska", result.getAuthor());
     }
 
     @Test
     public void findByIdOkTitleTest() {
         Post result = repository.findById(1L);
-        Assert.assertNotNull(result);
         Assert.assertEquals("\"...niech \u015bwiat m\u00f3j si\u0119 zarumieni!\"", result.getTitle());
     }
+
+    @Test
+    public void findByIdNullTest() {
+        Post result = repository.findById(6L);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void findAllTest() {
+        List<Post> result = repository.findAllByOrderByCreationDateDesc();
+        Assert.assertEquals(5, result.size());
+    }
+
+    @Test
+    public void deleteFoundId(){
+        repository.delete(1L);
+        List<Post> result = repository.findAllByOrderByCreationDateDesc();
+        Assert.assertEquals(4, result.size());
+    }
+
+    @Test
+    public void deleteNotFoundId() {
+        exception.expect(EmptyResultDataAccessException.class);
+        repository.delete(6L);
+        List<Post> result = repository.findAllByOrderByCreationDateDesc();
+        Assert.assertEquals(5, result.size());
+    }
+
+
 }
---
+++ src/test/resources/pl/marczykm/domain/drop.sql
@@ -0,0 +1,2 @@
+drop table post if exists;
+drop table SYSTEM_LOB_STREAM if exists;
