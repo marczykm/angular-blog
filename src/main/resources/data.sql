a15d9223d1984942601d0add19c89e9926ca220b
Marcin Marczyk <marcin.marczyk@capgemini.com>
2016-01-27 10:15:14 +0100

    data.sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO post
+(id,          title,           content, author,         creation_date, active) VALUES
+( 1, 'test title 1',  'test content 1', 'Anna', '2015-01-01 12:00:00',   true),
+( 2, 'test title 2',  'test content 2', 'Anna', '2015-01-01 12:00:00',   true),
+( 3, 'test title 3',  'test content 3', 'Anna', '2015-01-01 12:00:00',   true);
